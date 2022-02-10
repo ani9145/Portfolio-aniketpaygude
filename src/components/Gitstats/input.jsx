63b4c23f2c9f9da0c18a95d2e42d0d60df47b784
ani9145 <93372159+ani9145@users.noreply.github.com>
2022-02-10 19:53:52 +0530

    done

--- src/components/Gitstats/input.jsx
+++ src/components/Gitstats/input.jsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 export const Input=()=>{
     const [gamename,setGamename]=useState("")
     const [gameauthor, setGameauthor]=useState("")
-    const [gameprice,setGameprice]=useState("");
     const [gametags,setGametags]=useState("")
 return(
     <div>
@@ -14,15 +13,12 @@ return(
             onChange={(e)=> setGamename(e.target.value)} required></input>
     <input type="mail" id="mailid" name="Email" placeholder="Email Address" value={gameauthor}
            onChange={(e)=>setGameauthor(e.target.value)} required></input>
-    <input type="text" id="sub" name="Subject" placeholder="Subject"  value={gameprice}
-           onChange={(e)=>setGameprice(e.target.value)} required></input>
     <textarea id="msg" name="message" placeholder="Message" value={gametags}
            onChange={(e)=>setGametags(e.target.value)} required></textarea>
    <button style={{cursor: 'pointer'}} type="submit" onClick={(e)=>{
     setGamename("")
     setGameauthor("")
-    setGameprice("")
-    setGametags("")
+      setGametags("")
    }} >Send Message</button>
  </div>
  </div>
